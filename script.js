a34c86e2e60017768ab9a5a37f5f62c7faf79f63
Zhyar <zhiarali595@gmail.com>
2026-09-06 20:48:37 +0300

    finish buildTree function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const Node = function(dataInput) {
 };
 
 const Tree = function(array) {
-    
+
     let arr = array.sort((a,b) => a-b);
     for(let i=1; i<arr.length; i++) {
         let cur = arr[i];
@@ -19,14 +19,22 @@ const Tree = function(array) {
         }
     }
 
-    let root = buildTree(arr);
+    let root = buildTree(arr, 0, arr.length-1);
 
-    function buildTree(array) {
-        return array;
+    function buildTree(array, start, end) {
+        if(start>end) return null;
+
+        let mid = start + Math.floor((end-start) / 2);
+        let root = new Node(array[mid]);
+
+        root.left = buildTree(array, start, mid-1);
+        root.right = buildTree(array, mid+1, end)
+
+        return root;
     };
 
     return(root);
 };
 
-let tree =  Tree([5,2,3,2,1,2,2,5,3]);
+let tree = new Tree([5,2,3,2,1,2,2,5,3]);
 console.log(tree);
