1bdac1ad27ef9cbaf41ecb6cc127f83dd6204873
Zhyar <zhiarali595@gmail.com>
2026-09-06 20:40:50 +0300

    sort array before pass in buildTree function

--- script.js
+++ script.js
@@ -7,5 +7,26 @@ const Node = function(dataInput) {
 };
 
 const Tree = function(array) {
+    
+    let arr = array.sort((a,b) => a-b);
+    for(let i=1; i<arr.length; i++) {
+        let cur = arr[i];
+        let prev = arr[i-1];
 
+        if(cur === prev) {
+            arr.splice(i, 1);
+            i -=1;
+        }
+    }
+
+    let root = buildTree(arr);
+
+    function buildTree(array) {
+        return array;
+    };
+
+    return(root);
 };
+
+let tree =  Tree([5,2,3,2,1,2,2,5,3]);
+console.log(tree);
